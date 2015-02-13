f4b8f662bf69af54d16c8361d51bbc13f15dc144
raykin <raykincoldxiao@gmail.com>
2015-02-13 18:44:23 +0800

    change user login page style

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def self.create_test_user(email: 'ray@test.com')
+    create!(email: email, password: 'asdfasdf', password_confirmation: 'asdfasdf')
+  end
 end
